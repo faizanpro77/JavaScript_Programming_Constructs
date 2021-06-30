31e4f12318c9a6e888a954ca94a3eb8f7ae50d88
faizanpro77 <faizyshaikh204@gmail.com>
2021-07-01 01:31:54 +0530

    ADDED: add selectio if else if& else

---
+++ Selection if&else/selectionifelseif&else.js/readNumber.js
@@ -0,0 +1,18 @@
+//Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
+
+const prompt = require('prompt-sync')();
+
+let num = prompt("Enter a number : ");
+
+if (num == 1)
+    console.log("unit");
+else if (num == 10)
+    console.log("Ten");
+else if (num == 100)
+    console.log("Hundred");
+else if (num == 1000)
+    console.log("Thousand");
+else if (num == 10000)
+    console.log("Ten thousand");
+else
+    console.log("Invalid number");
